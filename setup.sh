45f77cb4b7c665e422e408c7502affbffb942fa0
igor almeida <igor.p.almeida@gmail.com>
2016-09-15 22:49:32 -0300

    canditate

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@ files=( "ps1.sh" "alias.sh" "helpers.sh" "export.sh" "git-completion.bash" "git-
 bundle=""
 for file in "${files[@]}"
 do
-  echo "=> checking file $HOME$file"
-  if [[ -e $HOME$file ]]; then
-    mv $HOME$file{,.bkp.$(date +%s)} -v
-	fi
   bundle="https://raw.githubusercontent.com/hankpillow/dotfiles/master/$file $bundle"
 done
-curl -O $bundle >> bash_profile
+if [[ -e $HOME/.bash_profile ]];
+then
+  cp $HOME/.bash_profile "$HOME/.bash_profile.bkp-$(date +%s)"
+fi
+curl -O $bundle > $HOME/.bash_profile
+cat $HOME/.bash_profile
