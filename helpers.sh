6449fa4dd57b0bff1279d6a3b99124511c22ef75
igor almeida <igor.p.almeida@gmail.com>
2016-09-15 22:31:58 -0300

    another attempt

--- helpers.sh
+++ helpers.sh
@@ -1,21 +1,17 @@
 function ip(){
-echo "$(ifconfig $1 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://')"
+  echo "$(ifconfig $1 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://')"
 }
 function rsa-create(){
-	ssh-keygen -t rsa -b 4096 -C "$1"
-	rsa-send-to-agent
-	eval "$(ssh-agent -s)"
-	ssh-add ~/.ssh/id_rsa
+  ssh-keygen -t rsa -b 4096 -C "$1"
+  rsa-send-to-agent
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
 }
 
 function docker-clear(){
-	docker rm -f "$(docker ps -a -q)"
-}
-
-function docker-create(){
-	docker-machine create --driver virtualbox $1
+  docker rm -f "$(docker ps -a -q)"
 }
 
 function pacman-stop(){
-	rm /var/lib/pacman/db.lck
+  rm /var/lib/pacman/db.lck
 }
--- setup.sh
+++ setup.sh
@@ -1,18 +1,11 @@
-while IFS= read -r file
+files=( "git-completion.bash" "git-prompt.bash" "docker-machine.bash" "docker-machine-prompt.bash" "ps1.sh" "alias.sh" "helpers.sh" "export.sh" )
+bundle=""
+for file in "${files[@]}"
 do
   if [[ -e $HOME$file ]]; then
     mv $HOME$file{,.bkp.$(date +%s)} -v
 	fi
-  curl "https://raw.githubusercontent.com/hankpillow/dotfiles/master/$file" > "$home$file"
-  #create an option for local copy
-  #cp $file $HOME$file -v
-done <<- EOM
-git-completion.bash
-git-prompt.bash
-docker-machine.bash
-docker-machine-prompt.bash
-ps1.sh
-alias.sh
-helper.sh
-export.sh
-EOM
+  bundle="https://raw.githubusercontent.com/hankpillow/dotfiles/master/$file $bundle"
+done
+echo "$bundle"
+curl -O "$bundle" >> bash_profile
