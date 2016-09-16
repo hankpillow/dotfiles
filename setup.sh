6f14758946c79d282f2f16ad79e9b0418946cb76
igor almeida <igor.p.almeida@gmail.com>
2016-09-15 22:44:39 -0300

    almost there

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-files=( "git-completion.bash" "git-prompt.bash" "docker-machine.bash" "docker-machine-prompt.bash" "ps1.sh" "alias.sh" "helpers.sh" "export.sh" )
+files=( "ps1.sh" "alias.sh" "helpers.sh" "export.sh" "git-completion.bash" "git-prompt.bash" "docker-machine.bash" "docker-machine-prompt.bash" )
 bundle=""
 for file in "${files[@]}"
 do
+  echo "=> checking file $HOME$file"
   if [[ -e $HOME$file ]]; then
     mv $HOME$file{,.bkp.$(date +%s)} -v
 	fi
   bundle="https://raw.githubusercontent.com/hankpillow/dotfiles/master/$file $bundle"
 done
-echo "$bundle"
-curl -O "$bundle" >> bash_profile
+curl -O $bundle >> bash_profile
