8c979213904b48635c824e8e2ade1285ec29e12a
hankpillow <ialmeida@fbiz.com.br>
2016-09-20 19:05:51 -0300

    helper atualizado

--- helpers.sh
+++ helpers.sh
@@ -10,6 +10,10 @@ function setup-vim(){
   curl "https://raw.githubusercontent.com/hankpillow/dotfiles/master/setup-vim.sh" | bash
 }
 
+function update-vim(){
+  curl "https://raw.githubusercontent.com/hankpillow/dotfiles/master/vimrc" > $HOME/.vimrc
+}
+
 function ip(){
   echo "$(ifconfig $1 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://')"
 }
