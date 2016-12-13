1674e961c791e92c0f41790d8cd867481ad54fd8
igor almeida <ialmeida@fbiz.com.br>
2016-12-13 19:50:08 -0200

    no cache para setup-profile

--- setup-profile.sh
+++ setup-profile.sh
@@ -8,6 +8,6 @@ if [[ -e $HOME/.bash_profile ]];
 then
   cp -v $HOME/.bash_profile "$HOME/.bash_profile.bkp-$(date +%s)"
 fi
-curl -O $bundle > $HOME/.bash_profile
+curl -H "Cache-Control: no-cache" -O $bundle > $HOME/.bash_profile
 cat $HOME/.bash_profile
 bash $HOME/.bash_profile
