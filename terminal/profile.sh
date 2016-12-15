375233d530f8dfea48f72b9ef7f98208d4fedd1a
Igor Almeida <igor.p.almeida@gmail.com>
2016-12-15 16:00:31 -0200

    .gitconfig instead of flags via profile

--- terminal/profile.sh
+++ terminal/profile.sh
@@ -2,11 +2,6 @@
 # profile.sh
 #
 
-alias ga="git add"
-alias gc="git commit"
-alias gf="git fetch --all --prune"
-alias gs="git status -s"
-
 alias l="ls -olahF"
 alias ccal='cal | sed -e "s/ $(expr `date "+%d"` + 0) / $(printf "\033[32m$(expr `date "+%d"` + 0)\033[0m") /"'
 alias vi='vim'
@@ -20,6 +15,7 @@ if [ "$(uname)" == "Darwin" ]; then
   shopt -s cdspell
 
   # MAC OS stuff
+  defaults write com.apple.finder AppleShowAllFiles YES
   defaults write com.apple.Preview NSQuitAlwaysKeepsWindows -bool false
   defaults write com.apple.QuickTimePlayerX NSQuitAlwaysKeepsWindows -bool false
   defaults write com.apple.finder QLEnableTextSelection -bool true
@@ -40,21 +36,3 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   alias ls='ls --color=auto'
   alias ya='yaourt'
 fi
-
-git config --global alias.tags 'log --tags --simplify-by-decoration --pretty="format:%ai %d"'
-git config --global alias.unstage 'reset HEAD --'
-git config --global alias.unmerged 'ls-files -u'
-git config --global alias.co checkout
-git config --global alias.ci commit
-git config --global alias.st status
-git config --global push.default simple
-git config --global alias.hist 'log --pretty=format:"%Cred%h%Creset	%ad	[%aN]	%s%d " --graph --date=short --abbrev-commit'
-git config --global color.branch auto
-git config --global color.diff auto
-git config --global color.interactive auto
-git config --global color.status auto
-git config --global diff.tool opendiff
-git config --global difftool.prompt false
-git config --global merge.tool opendiff
-git config --global merge.conflictstyle diff3
-
