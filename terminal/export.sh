1e124080940038d7dba032ab09525adfba60fb4f
Igor Almeida <igor.p.almeida@gmail.com>
2017-05-24 10:51:42 -0300

    workspace env var created

--- terminal/export.sh
+++ terminal/export.sh
@@ -6,6 +6,13 @@ export LC_ALL="en_US.UTF-8"
 export LANG="en_US"
 export HOME=`echo ~/`
 
+# create a default workspace at user's HOME directory
+if [ ! -d "$HOME/workspace" ]; then
+  echo "creating workspace folder at $HOME"
+  mkdir -v $HOME/workspace
+fi
+export WORKSPACE=`echo $HOME/workspace`
+
 export EDITOR="vim"
 export MANPAGER="less -X"
 
@@ -20,3 +27,6 @@ export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/libexec:$PATH"
 
+if [ -d "$WORKSPACE/google-cloud-sdk/bin" ]; then
+  export PATH="$WORKSPACE/google-cloud-sdk/bin:$PATH"
+fi
