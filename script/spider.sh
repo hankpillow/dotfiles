55fad52d768a28a1bc01dd47b7c8e5b1f0cc627c
Igor Almeida <igor.p.almeida@gmail.com>
2017-06-29 19:02:46 -0300

    spider trying to no set certificates

--- script/spider.sh
+++ script/spider.sh
@@ -7,15 +7,19 @@ AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Fire
 DEPTH=1
 DRY=0
 
-function dump {
-  echo ""
-	echo "# Running crawler"
-	echo "# page: $PAGE"
-	echo "# domain: $DOMAIN"
-	echo "# referer: http://$REFERER"
-	echo "# depth: $DEPTH"
-  echo "$ agent: $AGENT"
-  echo ""
+bold=$(tput bold)
+normal=$(tput sgr0)
+function help {
+  echo -e ""
+  echo -e "${bold}spider.sh${normal} is a simple interface for most commom usage of wget as crawler"
+  echo -e "Usage: spider.sh <URL> [OPTION]"
+  echo -e ""
+  echo -e "${bold}Options:"
+  echo -e " ${bold}--domain${normal}\tDomain name to feed wget's -D. Default is extrating the domain from target URL"
+  echo -e " ${bold}--agent${normal}\tUser agent to feed wget's --user-agent. Default is Mozilla"
+  echo -e " ${bold}--depth${normal}\tDefine how deep the crawler can go in (wget's -l param). Default is 1"
+  echo -e " ${bold}--referer${normal}\tRequest Referer (wget's --referer param). Default is extrating referer from target URL"
+  echo -e " ${bold}--dry${normal}  \tJust dump the command without calling it. Good to see/edit before calling"
 }
 
 for i in "$@"
@@ -42,32 +46,25 @@ do
 			shift
 			;;
 			-help|--help|-h)
-			bold=$(tput bold)
-			normal=$(tput sgr0)
-			echo -e ""
-			echo -e "${bold}spider.sh${normal} is a simple interface for most commom usage of wget as crawler"
-			echo -e "Usage: spider.sh <URL> [OPTION]"
-			echo -e ""
-			echo -e "${bold}Options:"
-			echo -e "\t ${bold}--domain${normal}\tDomain name to feed wget's -D. Default is extrating the domain from target URL"
-			echo -e "\t ${bold}--agent${normal}\tUser agent to feed wget's --user-agent. Default is Mozilla"
-			echo -e "\t ${bold}--depth${normal}\tDefine how deep the crawler can go in (wget's -l param). Default is 1"
-			echo -e "\t ${bold}--referer${normal}\tRequest Referer (wget's --referer param). Default is extrating referer from target URL"
-			echo -e "\t ${bold}--dry${normal}  \tJust dump the command without calling it. Good to see/edit before calling"
+      help
 			exit
 			;;
 	esac
 done
 
-dump
-
-CMD="wget --spider -r -l$DEPTH --max-redirect=10 --force-html -H -D$DOMAIN -nd -np -S --referer=http://$REFERER --user-agent=\"$AGENT\" $PAGE"
-# CMD="wget --spider -r -l$DEPTH --max-redirect=10 --force-html -H -D$DOMAIN -nd -np -S --referer=http://$REFERER --user-agent=\"$AGENT\" $PAGE 2>&1"
-
-if [[ $DRY == 0 ]];
+if [[ $PAGE == "" ]];
 then
-	eval $CMD 2>&1
+  help
+
 else
-  echo "Run:"
-	echo "$CMD"
+  CMD="wget --spider -r -l$DEPTH --no-check-certificate --max-redirect=10 --force-html -H -D$DOMAIN -nd -np -S --referer=http://$REFERER --user-agent=\"$AGENT\" $PAGE"
+
+  if [[ $DRY == 0 ]];
+  then
+    eval $CMD 2>&1
+  else
+    echo -e "${bold}Run:${normal}"
+    echo "$CMD"
+  fi
+
 fi
