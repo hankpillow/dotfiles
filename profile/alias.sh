1cc0d34f253e5e0144e9c6e31478fbfc8fc607f0
Igor Almeida <igor.p.almeida@gmail.com>
2018-03-20 15:23:28 +0000

    ubuntu working

--- profile/alias.sh
+++ profile/alias.sh
@@ -2,22 +2,14 @@
 # alias.sh
 #
 
-# MacOS
-if [ "$(uname)" == "Darwin" ]; then
+alias ls='ls -G'
 
-	alias ls='ls -G'
-	alias mac_refresh_camera='killall VDCAssistant'
-	alias mac_refresh_finder='killall Finder'
-	alias ccal='cal | sed -e "s/ $(expr `date "+%d"` + 0) / $(printf "\033[32m$(expr `date "+%d"` + 0)\033[0m") /"'
-  alias reload_profile='bash ~/.bash_profile'
+alias mac_refresh_camera='killall VDCAssistant'
+alias mac_refresh_finder='killall Finder'
 
-# Linux (working on Arch)
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+alias ccal='cal | sed -e "s/ $(expr `date "+%d"` + 0) / $(printf "\033[32m$(expr `date "+%d"` + 0)\033[0m") /"'
 
-	alias ls='ls --color=auto'
-
-fi
-
+alias reload_profile='bash ~/.bash_profile'
 alias remove_bkp='rm -v ~/.*.bkp'
 
 alias cp='cp -iv'
@@ -40,51 +32,15 @@ alias create_rsa='mkdir ~/.ssh && ssh-keygen -t rsa -b 4096 -C "$1" && rsa-send-
 #   ---------------------------
 
 alias netCons='lsof -i'                             # netCons:      Show all open TCP/IP sockets
-alias lsock='sudo /usr/sbin/lsof -i -P'             # lsock:        Display open sockets
-alias lsockU='sudo /usr/sbin/lsof -nP | grep UDP'   # lsockU:       Display only open UDP sockets
-alias lsockT='sudo /usr/sbin/lsof -nP | grep TCP'   # lsockT:       Display only open TCP sockets
+# alias lsock='sudo /usr/sbin/lsof -i -P'             # lsock:        Display open sockets
+# alias lsockU='sudo /usr/sbin/lsof -nP | grep UDP'   # lsockU:       Display only open UDP sockets
+# alias lsockT='sudo /usr/sbin/lsof -nP | grep TCP'   # lsockT:       Display only open TCP sockets
 alias myip='curl ifconfig.co'
-alias ipInfo0='ipconfig getpacket en0'              # ipInfo0:      Get info on connections for en0
-alias ipInfo1='ipconfig getpacket en1'              # ipInfo1:      Get info on connections for en1
-alias openPorts='lsof -Pn -i4 | grep LISTEN'
+# alias ipInfo0='ipconfig getpacket en0'              # ipInfo0:      Get info on connections for en0
+# alias ipInfo1='ipconfig getpacket en1'              # ipInfo1:      Get info on connections for en1
+# alias openPorts='lsof -Pn -i4 | grep LISTEN'
 # alias openPorts='sudo lsof -i | grep LISTEN'        # openPorts:    All listening connections
-alias showBlocked='sudo ipfw list'                  # showBlocked:  All ipfw rules inc/ blocked IPs
-
-#   ---------------------------
-#   5. PROCESS MANAGEMENT
-#   ---------------------------
-
-#   findPid: find out the pid of a specified process
-#   -----------------------------------------------------
-#       Note that the command name can be specified via a regex
-#       E.g. findPid '/d$/' finds pids of all processes with names ending in 'd'
-#       Without the 'sudo' it will only find processes of the current user
-#   -----------------------------------------------------
-findPid () { lsof -t -c "$@" ; }
-
-#   memHogsTop, memHogsPs:  Find memory hogs
-#   -----------------------------------------------------
-alias memHogsTop='top -l 1 -o rsize | head -20'
-alias memHogsPs='ps wwaxm -o pid,stat,vsize,rss,time,command | head -10'
-
-#   cpuHogs:  Find CPU hogs
-#   -----------------------------------------------------
-alias cpu_hogs='ps wwaxr -o pid,stat,%cpu,time,command | head -10'
-
-#   topForever:  Continual 'top' listing (every 10 seconds)
-#   -----------------------------------------------------
-alias topForever='top -l 9999999 -s 10 -o cpu'
-
-#   ttop:  Recommended 'top' invocation to minimize resources
-#   ------------------------------------------------------------
-#       Taken from this macosxhints article
-#       http://www.macosxhints.com/article.php?story=20060816123853639
-#   ------------------------------------------------------------
-alias ttop="top -R -F -s 10 -o rsize"
-
-#   my_ps: List processes owned by my user:
-#   ------------------------------------------------------------
-my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,start,time,bsdtime,command ; }
+# alias showBlocked='sudo ipfw list'                  # showBlocked:  All ipfw rules inc/ blocked IPs
 
 #   extract:  Extract most know archives with one command
 #   ---------------------------------------------------------
@@ -172,4 +128,4 @@ dbu() { docker build -t=$1 .; }
 dalias(){ cat ~/.bash_profile | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | grep -v "#" | sort;}
 
 # Bash into running container
-dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+dbash() { docker exec -it $(docker ps -aqf "name=$2") bash; }
