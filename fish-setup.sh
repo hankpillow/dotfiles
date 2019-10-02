fb5722c00be2dd5a3d1e3d632b0301157b57cb8b
Igor Almeida <igor.p.almeida@gmail.com>
2019-10-02 09:10:33 -0300

    ajustando fish setup

--- fish-setup.sh
+++ fish-setup.sh
@@ -3,13 +3,6 @@
 rm -rvf ~/.config/fish
 mkdir -vp ~/.config/fish/functions
 
-if [[ ! $(command -v fisher) ]]; then
-    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-fi
-
-echo install fish-shell addons for copy and paste
-curl --silent "https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/functions/fish_clipboard_copy.fish"  > ~/.config/fish/functions/fish_clipboard_copy.fish
-curl --silent "https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/functions/fish_clipboard_paste.fish"  > ~/.config/fish/functions/fish_clipboard_paste.fish
 
 source fish-update-config.sh
 
@@ -18,7 +11,7 @@ if [[ ! $(command -v fish) ]]; then
   sudo apt-get install fish --fix-missing
 fi
 
-fish
-fisher add fishpkg/fish-git-util
-fisher add jethrokuan/fzf
+if [[ ! $(command -v fisher) ]]; then
+    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fi
 
--- fish-update-config.sh
+++ fish-update-config.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo install fish-shell addons for copy and paste
+curl --silent "https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/functions/fish_clipboard_copy.fish"  > ~/.config/fish/functions/fish_clipboard_copy.fish
+curl --silent "https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/functions/fish_clipboard_paste.fish"  > ~/.config/fish/functions/fish_clipboard_paste.fish
+
+fisher add fishpkg/fish-git-util
+fisher add jethrokuan/fzf
+
 # Send local
 cp -v config/fish/functions/*.fish ~/.config/fish/functions/
 cp -v config/fish/config.fish ~/.config/fish/
