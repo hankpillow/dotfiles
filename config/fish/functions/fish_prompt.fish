ccc929c6c4cfae40e048fbb0105219aab17dad96
Igor Almeida <igor.p.almeida@gmail.com>
2019-10-03 09:24:30 -0300

    no bold

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -4,22 +4,22 @@ function fish_prompt
   if test $last_status -ne 0
     set status_color (set_color red -o)
   else
-    set status_color (set_color cyan -o)
+    set status_color (set_color cyan)
   end
   echo -n -s $status_color (date '+%T')
 
   # jobs
   set -l runningjobs (jobs | wc -l | grep -oh "[0-9]")
-  echo -n -s (set_color normal -o) " " $runningjobs
+  echo -n -s (set_color normal) " " $runningjobs
 
   # working dir
   set -l cwd (prompt_pwd)
-  echo -n -s " " (set_color green -o) $cwd ""
+  echo -n -s " " (set_color green) $cwd ""
 
   # git branch
 
   if test -e  $HOME/.config/fish/functions/git_is_repo.fish && git_is_repo
-    echo -n -s " " (set_color white -o) "(" (git_branch_name) ")"
+    echo -n -s " " (set_color white -o) (git_branch_name) 
   end
 
   # done
