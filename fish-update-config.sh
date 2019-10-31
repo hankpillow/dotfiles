3bd2d697dab6b4fe529c9d8230d1a6eadf61a921
Igor Almeida <igor.p.almeida@gmail.com>
2019-10-31 14:32:05 -0300

    limpando coisas velhas antes do update

--- fish-update-config.sh
+++ fish-update-config.sh
@@ -4,10 +4,15 @@ echo install fish-shell addons for copy and paste
 curl --silent "https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/functions/fish_clipboard_copy.fish"  > ~/.config/fish/functions/fish_clipboard_copy.fish
 curl --silent "https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/functions/fish_clipboard_paste.fish"  > ~/.config/fish/functions/fish_clipboard_paste.fish
 
+if [[ ! $(command -v fisher) ]]; then
+    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fi
+
 fisher add fishpkg/fish-git-util
 fisher add jethrokuan/fzf
-
+#
 # Send local
+find ~/.config/* -type f -iname "*.fish" | grep -v fisher  | xargs rm
 cp -v config/fish/functions/*.fish ~/.config/fish/functions/
 cp -v config/fish/config.fish ~/.config/fish/
 
