a3660eb95aad312830d2f1d7ba3eccf738f93655
igor <igor.p.almeida@gmail.com>
2020-03-06 16:35:18 -0300

    fisher ok

--- fish-setup.sh
+++ fish-setup.sh
@@ -2,18 +2,5 @@
 # Grant .config/fish/functions before..
 rm -rvf ~/.config/fish
 mkdir -vp ~/.config/fish/functions
-
-source fish-update-config.sh
-
-if [[ ! $(command -v fish) ]]; then
-  echo "Install fish first"
-  sudo add-apt-repository ppa:fish-shell/release-3
-  sudo apt-get install fish --fix-missing
-fi
-
-if [[ ! $(command -v fisher) ]]; then
-    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-fi
-
-fisher
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
--- fish-update-config.fish
+++ fish-update-config.fish
@@ -1,15 +1,10 @@
-#!/bin/bash
-
 echo install fish-shell addons for copy and paste
 curl --silent "https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/functions/fish_clipboard_copy.fish"  > ~/.config/fish/functions/fish_clipboard_copy.fish
 curl --silent "https://raw.githubusercontent.com/fish-shell/fish-shell/master/share/functions/fish_clipboard_paste.fish"  > ~/.config/fish/functions/fish_clipboard_paste.fish
 
-if [[ ! $(command -v fisher) ]]; then
-    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-fi
-
-fisher add fishpkg/fish-git-util
-fisher add jethrokuan/fzf
+chmod +x ~/.config/fish/functions/*.fish
+fish fisher add fishpkg/fish-git-util
+fish fisher add jethrokuan/fzf
 #
 # Send local
 find ~/.config/* -type f -iname "*.fish" | grep -v fisher  | xargs rm
@@ -17,4 +12,3 @@ cp -v config/fish/functions/*.fish ~/.config/fish/functions/
 cp -v config/fish/config.fish ~/.config/fish/
 
 # Grant proper permissions
-chmod +x ~/.config/fish/functions/*.fish
