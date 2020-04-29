9c1f8707b9b1e7047aa948196791dd2eb5fb8b17
igor <igor.p.almeida@gmail.com>
2020-04-29 14:09:44 -0300

    setting new docker image

--- config/fish/functions/decrypt.fish
+++ config/fish/functions/decrypt.fish
@@ -1,4 +1,7 @@
 function decrypt
-    # command openssl aes-256-cbc -a -d -md sha256
-    command docker run --rm -v $HOME/workspace/minivault/test/:/test -v $HOME/workspace/minivault/bin/:/cli -v $HOME/workspace/minivault/extension/:/extension minivault/puppeteer:3.0.2 node cli/decrypt.js $argv
+    command docker run --rm minivault/decrypt:1.5.0 $argv
 end
+# function decrypt
+#     # command openssl aes-256-cbc -a -d -md sha256
+#     command docker run --rm -v $HOME/workspace/minivault/test/:/test -v $HOME/workspace/minivault/bin/:/cli -v $HOME/workspace/minivault/extension/:/extension minivault/puppeteer:3.0.2 node cli/decrypt.js $argv
+# end
--- config/fish/functions/encrypt.fish
+++ config/fish/functions/encrypt.fish
@@ -1,4 +1,7 @@
 function encrypt
-    # command openssl aes-256-cbc -a -e -md sha256
-    command docker run --rm -v $HOME/workspace/minivault/test/:/test -v $HOME/workspace/minivault/bin/:/cli -v $HOME/workspace/minivault/extension/:/extension minivault/puppeteer:3.0.2 node cli/encrypt.js $argv
+    command docker run --rm minivault/encrypt:1.5.0 $argv
 end
+# function encrypt
+#     # command openssl aes-256-cbc -a -e -md sha256
+#     command docker run --rm -v $HOME/workspace/minivault/test/:/test -v $HOME/workspace/minivault/bin/:/cli -v $HOME/workspace/minivault/extension/:/extension minivault/puppeteer:3.0.2 node cli/encrypt.js $argv
+# end
