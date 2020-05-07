3e20f31b042a67ecc5cc54aa8c22c3c04532d43c
igor <igor.p.almeida@gmail.com>
2020-05-07 11:53:04 -0300

    fix minivault

--- config/fish/functions/decrypt.fish
+++ config/fish/functions/decrypt.fish
@@ -1,7 +1,4 @@
 function decrypt
-    command docker run --rm minivault/decrypt:1.5.0 $argv
+    command docker run --rm minivault/vault decrypt $argv
+     # command openssl aes-256-cbc -a -d -md sha256
 end
-# function decrypt
-#     # command openssl aes-256-cbc -a -d -md sha256
-#     command docker run --rm -v $HOME/workspace/minivault/test/:/test -v $HOME/workspace/minivault/bin/:/cli -v $HOME/workspace/minivault/extension/:/extension minivault/puppeteer:3.0.2 node cli/decrypt.js $argv
-# end
--- config/fish/functions/encrypt.fish
+++ config/fish/functions/encrypt.fish
@@ -1,7 +1,4 @@
 function encrypt
-    command docker run --rm minivault/encrypt:1.5.0 $argv
+    command docker run --rm minivault/vault encrypt $argv
+    # command openssl aes-256-cbc -a -e -md sha256
 end
-# function encrypt
-#     # command openssl aes-256-cbc -a -e -md sha256
-#     command docker run --rm -v $HOME/workspace/minivault/test/:/test -v $HOME/workspace/minivault/bin/:/cli -v $HOME/workspace/minivault/extension/:/extension minivault/puppeteer:3.0.2 node cli/encrypt.js $argv
-# end
