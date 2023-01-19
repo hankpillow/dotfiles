d65973a09c61841dfb4dc08ec80588103ce2c02a
Igor Almeida <igor.p.almeida@gmail.com>
2023-01-19 14:42:10 -0300

    remove map

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,27 +1,26 @@
-require('telescope').setup({
-	file_ignore_patterns = {
-		"node_modules",
-		".cache",
-		"dist",
-		"public"
-	}
+require("telescope").setup({
+    file_ignore_patterns = {
+        "node_modules",
+        ".cache",
+        "dist",
+        "public"
+    }
 })
 
 -- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
+pcall(require("telescope").load_extension, "fzf")
 
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', '<C-p>', function()
-    builtin.git_files({previewer = false})
+vim.keymap.set("n", "<C-p>", function()
+    builtin.git_files({ previewer = false })
 end, {})
-vim.keymap.set('n', '<A-p>', function()
-    builtin.find_files({previewer = false})
+
+vim.keymap.set("n", "<A-p>", function()
+    builtin.find_files({ previewer = false })
 end, {})
-vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
-vim.keymap.set('n', '<space><space>', builtin.buffers, {})
-vim.keymap.set('n', '<space>?', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>br', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+
+vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
+vim.keymap.set("n", "<space><space>", builtin.buffers, {})
+vim.keymap.set("n", "<space>?", builtin.oldfiles, {})
+vim.keymap.set("n", "<leader>br", builtin.git_branches, {})
