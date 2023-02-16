21ba9f4b9d3a2013f30a5ff20dad83be361b710e
Igor Almeida <igor.p.almeida@gmail.com>
2023-02-16 13:40:37 -0300

    update tmux

--- git2home.sh
+++ git2home.sh
@@ -3,6 +3,7 @@
 # cp -fv .gitconfig ~/
 # cp -fv .gitignore ~/
 cp -fv .*rc ~/
+cp -fv tmux/* ~/
 
 rsync -rahv fish/ ~/.config/fish --exclude completions --exclude fish_variables
 rsync -rav --delete nvim/** ~/.config/nvim  
