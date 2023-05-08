2745691a6f427e15e7cbbbdca8aad88d4f579cf4
Igor Almeida <Igor_Almeida@dell.com>
2023-05-08 16:05:50 -0300

    map git fugitive

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,3 +1,11 @@
 vim.cmd([[
 set statusline=%<%f\ %h%m%r%{FugitiveStatusline()}%=%y\ %(%l,%c%)\ %k%q
 ]])
+
+local keymap = vim.keymap
+local e_opts = { noremap = true, expr = true }
+local s_opts = { noremap = true, silent = true }
+local se_opts = { noremap = true, silent = true, expr = true }
+
+-- Exit
+keymap.set('n', '<leader>g', ':0G', e_opts) -- quit
