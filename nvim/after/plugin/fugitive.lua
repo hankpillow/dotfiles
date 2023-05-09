f2c9ee2031a45b4dc208f6a0e942d1da02a6abeb
Igor Almeida <Igor_Almeida@dell.com>
2023-05-09 09:34:30 -0300

    mapping 0G

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
 set statusline=%<%f\ %h%m%r%{FugitiveStatusline()}%=%y\ %(%l,%c%)\ %k%q
 nnoremap <leader>git :0G<CR>
-nnoremap <space>gps :Dispatch! git push<CR>
-nnoremap <space>gpl :Dispatch! git pull<CR>
+nnoremap <leader>gps :! git push<CR>
+nnoremap <leader>gpl :! git pull<CR>
 ]])
