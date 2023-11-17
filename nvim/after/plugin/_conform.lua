a295496a1322616dbae5f2b3857932db70d27e60
hankpillow <igor.p.almeida@gmail.com>
2023-11-17 13:54:47 -0300

    teak format

--- nvim/after/plugin/_conform.lua
+++ nvim/after/plugin/_conform.lua
@@ -6,20 +6,34 @@ end
 
 -- https://github.com/stevearc/conform.nvim
 plugin.setup({
+    log_level = vim.log.levels.ALL,
     formatters_by_ft = {
         javascript = { { "prettier" } },
         typescript = { { "prettier" } },
         typescriptreact = { { "prettier" } },
         html = { { "prettier" } },
         xml = { { "prettier" } },
-        scss = { { "stylelint" } },
+        scss = { { "prettier", "stylelint" } },
+        css = { { "prettier", "stylelint" } },
     },
-    notify_on_error = true
+    notify_on_error = true,
 })
 
 vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 
-vim.keymap.set("n", "<A-F>", function()
-    plugin.format({ async = true, lsp_fallback = true })
+vim.keymap.set({ "n", "v" }, "<A-F>", function()
+    plugin.format({ async = true, lsp_fallback = true, quiet = false })
+    -- local bufnr = vim.api.nvim_get_current_buf()
+    -- local formatters = plugin.list_formatters(bufnr)
+    -- print(vim.inspect(formatters))
+    -- print(formatters.name .. "--" .. formatters["name"])
+    -- print(type(formatters))
+    -- if formatters.name then
+    --     print("Formatting with " .. formatters.name)
+    --     -- plugin.format({ async = true, lsp_fallback = true, quiet = false })
+    -- else 
+    --     print("Formatting with vim defaults")
+    --     -- vim.cmd("normal! gg=G")
+    -- end
 end, { noremap = true, silent = false })
 
