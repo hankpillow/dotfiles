b1218dfec1d91d093dad4597c9554d48a8badc54
hankpillow <igor.p.almeida@gmail.com>
2023-12-22 14:12:06 -0300

    adding maps to grepper

--- nvim/after/plugin/_vim-grepper.lua
+++ nvim/after/plugin/_vim-grepper.lua
@@ -1,1 +1,7 @@
 -- grepper config
+vim.cmd[[
+nmap gs  <plug>(GrepperOperator)
+xmap gs  <plug>(GrepperOperator)
+nnoremap <leader>g :Grepper -noopen -side -tool git<cr>
+nnoremap <leader>G :Grepper -noopen -side -tool ag<cr>
+]]
