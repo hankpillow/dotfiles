726c0962c9a3e93d3db5c8aebe79b2a132289c72
Igor Almeida <Igor_Almeida@dell.com>
2024-02-06 09:18:36 -0300

    tweaking cmp to show copilot as ghost and autopcomplete in the popoup

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -15,17 +15,27 @@ return {
 			"hrsh7th/cmp-buffer", -- http://github.com/hrsh7th/cmp-buffer
 			"hrsh7th/cmp-cmdline", -- https://github.com/hrsh7th/cmp-cmdline
 			"L3MON4D3/LuaSnip", -- https://github.com/L3MON4D3/LuaSnip
-			"zbirenbaum/copilot.lua", -- https://github.com/zbirenbaum/copilot.lua
-			"zbirenbaum/copilot-cmp", -- https://github.com/zbirenbaum/copilot-cmp
+			{
+				"zbirenbaum/copilot.lua", -- https://github.com/zbirenbaum/copilot.lua
+				cmd = "Copilot",
+				build = ":Copilot auth",
+				event = "InsertEnter",
+			},
+			-- "zbirenbaum/copilot-cmp", -- https://github.com/zbirenbaum/copilot-cmp
 		},
 		config = function()
 			-- Copilot setup
 			require("copilot").setup({
-				cmd = "Copilot",
-				event = "InsertEnter",
-				suggestion = { enabled = false },
 				auto_trigger = false,
-				panel = { enabled = false },
+				panel = {
+					enabled = true,
+					auto_refresh = true,
+				},
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					accept = false, -- disable built-in keymapping
+				},
 				filetypes = {
 					["*"] = false,
 					javascript = true,
@@ -41,7 +51,7 @@ return {
 				},
 			})
 
-			require("copilot_cmp").setup()
+			-- require("copilot_cmp").setup()
 
 			local cmp = require("cmp")
 			cmp.setup({
@@ -67,15 +77,19 @@ return {
 					["<C-Space>"] = cmp.mapping.confirm({ select = true }),
 					["<CR>"] = nil,
 					["<S-Tab>"] = nil,
-					["<Tab>"] = nil,
+					-- ["<Tab>"] = nil,
 					-- ["<CR>"] = cmp.mapping.confirm({ select = true }),
-					-- ["<Tab>"] = vim.schedule_wrap(function(fallback)
-					-- 	if cmp.visible() and has_words_before() then
-					-- 		cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-					-- 	else
-					-- 		fallback()
-					-- 	end
-					-- end),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+						elseif require("copilot.suggestion").is_visible() then
+							require("copilot.suggestion").accept()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 			})
 
@@ -96,6 +110,14 @@ return {
 					{ name = "cmdline" },
 				}),
 			})
+
+			cmp.event:on("menu_opened", function()
+				vim.b.copilot_suggestion_hidden = true
+			end)
+
+			cmp.event:on("menu_closed", function()
+				vim.b.copilot_suggestion_hidden = false
+			end)
 		end,
 	},
 }
