3138b4662927f1781d91bc45c743cb05a77d251c
Igor Almeida <Igor_Almeida@dell.com>
2024-02-08 16:22:24 -0300

    cmd list visible buffers

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -60,7 +60,18 @@ return {
 					{ name = "nvim_lsp" },
 					{ name = "path" },
 					{ name = "nvim_lua" },
-					{ name = "buffer" },
+					{
+						name = "buffer",
+						option = {
+							get_bufnrs = function()
+								local bufs = {}
+								for _, win in ipairs(vim.api.nvim_list_wins()) do
+									bufs[vim.api.nvim_win_get_buf(win)] = true
+								end
+								return vim.tbl_keys(bufs)
+							end,
+						},
+					},
 				},
 				snippet = {
 					-- expand = function(args)
