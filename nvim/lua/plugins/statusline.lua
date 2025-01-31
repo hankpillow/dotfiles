d1230f7842f7c0af09d9d745962e6fd86794678b
Igor Almeida <Igor_Almeida@dell.com>
2025-01-31 14:45:50 -0300

    no buffers

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -13,16 +13,28 @@ return {
 			sections = {
 				lualine_a = {
 					{
-						"buffers",
-						use_mode_colors = true,
-						hide_filename_extension = false,
+						"filename",
+						file_status = true, -- Displays file status (readonly status, modified status)
+						newfile_status = false, -- Display new file status (new file means no write after created)
+						path = 1,
+						-- 0: Just the filename
+						-- 1: Relative path
+						-- 2: Absolute path
+						-- 3: Absolute path, with tilde as the home directory
+						-- 4: Filename and parent dir, with tilde as the home directory
+						-- shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+						-- for other components. (terrible name, any suggestions?)
 					},
 				},
 				lualine_b = {},
 				lualine_c = {},
 				lualine_x = { "branch", "diff" },
 				lualine_y = { "filesize" },
-				lualine_z = {},
+				lualine_z = { {
+					"searchcount",
+					maxcount = 999,
+					timeout = 500,
+				} },
 			},
 			inactive_sections = {
 				lualine_a = {},
